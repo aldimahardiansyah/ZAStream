912773a51b03aab6f04469d4c51e76e576471540
aldimahardiansyah <aldimdiansyah@gmail.com>
2021-12-12 08:55:25 +0700

    menambahakan home dan table animes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AnimeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+# menampilkan halaman home
+Route::get('/', [AnimeController::class, 'index']);
