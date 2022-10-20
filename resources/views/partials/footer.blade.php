d6e76be449c4fde3792b6e0d1b16d98e382b7d2b
aldimdiansyah <aldimdiansyah@gmail.com>
2022-10-20 09:37:53 +0700

    change year in footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="mt-3 bg-dark p-3" style="width: 100%;">
-    <small class="text-light">Copyright &copy; 2021 - <a href="/" class="text-decoration-none text-warning">ZAStream</a></small>
+    <small class="text-light">Copyright &copy; {{ date('Y') }} - <a href="/" class="text-decoration-none text-warning">ZAStream</a></small>
 </footer>
